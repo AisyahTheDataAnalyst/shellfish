d7ca91d8ae8e43404c10ca42ad4e8a5e1aab280d
Lumilu99 <yenwen_lu@hotmail.com>
2025-07-04 17:36:01 +0800

    Norminette done for check_input.c

--- src/check_input.c
+++ src/check_input.c
@@ -6,17 +6,61 @@
 /*   By: yelu <yelu@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 14:50:08 by yelu              #+#    #+#             */
-/*   Updated: 2025/06/27 23:09:14 by yelu             ###   ########.fr       */
+/*   Updated: 2025/07/04 17:28:40 by yelu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+static int	pipe_check(char **basin, int i)
+{
+	int	next_type;
+
+	if ((i == 0) || (basin[i + 1] == NULL))
+	{
+		ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 2);
+		return (0);
+	}
+	next_type = check_token_type(basin[i + 1]);
+	if (next_type == TOKEN_PIPE)
+	{
+		ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 2);
+		return (0);
+	}
+	return (1);
+}
+
+static int	redirect_check(char **basin, int i)
+{
+	int	next_type;
+
+	if (basin[i + 1] == NULL)
+	{
+		printf("bash: syntax error near unexpected token `%s'\n", basin[i]);
+		return (0);
+	}
+	next_type = check_token_type(basin[i + 1]);
+	if (next_type == TOKEN_APPEND || next_type == TOKEN_HEREDOC
+		|| next_type == TOKEN_REDIRECT_IN
+		|| next_type == TOKEN_REDIRECT_OUT || next_type == TOKEN_PIPE)
+	{
+		printf("bash: syntax error near unexpected token `%s'\n", basin[i]);
+		return (0);
+	}
+	return (1);
+}
+
+/// @brief Check input - To check if there is input right after any redirections
+// and if there are inputs before and after each pipe. Redirection operators
+// must not followed by any kind of logical operators
+// Invalid inputs: "< << infile", "< > outfile", "> >> outfile"
+// " | echo hello world", "echo hello | ".
+/// @param basin  Splitted array.
+/// @return Boolean, error message if invalid.
 int	check_input(char **basin)
 {
 	int	i;
 	int	type;
-	int	next_type;
 
 	i = 0;
 	while (basin[i])
@@ -24,33 +68,15 @@ int	check_input(char **basin)
 		type = check_token_type(basin[i]);
 		if (type == TOKEN_PIPE)
 		{
-			if ((i == 0) || (basin[i + 1] == NULL))
-			{
-				ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 2);
+			if (!pipe_check(basin, i))
 				return (0);
-			}
-			next_type = check_token_type(basin[i + 1]);
-			if (next_type == TOKEN_PIPE)
-			{
-				ft_putstr_fd("bash: syntax error near unexpected token `|'\n", 2);
-				return (0);
-			}
 		}
-		else if (type == TOKEN_APPEND || type == TOKEN_HEREDOC || type == TOKEN_REDIRECT_IN ||
-			type == TOKEN_REDIRECT_OUT)
+		else if (type == TOKEN_APPEND || type == TOKEN_HEREDOC
+			|| type == TOKEN_REDIRECT_IN
+			|| type == TOKEN_REDIRECT_OUT)
 		{
-			if (basin[i + 1] == NULL)
-			{
-				ft_putstr_fd("bash: syntax error near unexpected token `newline'\n", 2);
+			if (!redirect_check(basin, i))
 				return (0);
-			}
-			next_type = check_token_type(basin[i + 1]);
-			if (next_type == TOKEN_APPEND || next_type == TOKEN_HEREDOC || next_type == TOKEN_REDIRECT_IN ||
-			next_type == TOKEN_REDIRECT_OUT || next_type == TOKEN_PIPE)
-			{
-				ft_putstr_fd("bash: syntax error near unexpected token `newline'\n", 2);
-				return (0);
-			}
 		}
 		i++;
 	}
