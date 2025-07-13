b598db4d24dfceb4e901c29fe3f920e44a359ff3
wshee <weiyee1401@gmail.com>
2025-07-13 21:45:36 +0800

    refactor: break into functions, can handle multiple $

--- src/token/src/expansion/expansion.c
+++ src/token/src/expansion/expansion.c
@@ -1,97 +1,93 @@
 # include "../include/minishell.h"
 
-void parameter_expansion(t_token *token, char **env);
+void parameter_expansion(char **str, char **env);
 char *value_expansion(char *param, char **env);
+void get_value(char *param, char **env, char **result);
+
 
 void expand_tokens(t_token *token, char **env)
 {
-    parameter_expansion(token, env);
-	//handle exit code
-	//tilde epansion(optional)
-    //quote_removal
-}
-
-void parameter_expansion(t_token *token, char **env)
-{
-    char *param;
-	char *result;
-	char *value;
-	char *temp;
-	int start;
-
-    param = NULL;
-	value = NULL;
-	result = NULL;
-	temp = NULL;
-	start = 0;
-    while (token)
-    {
+	while (token)
+	{
 		int i = 0;
         while (token->basin_buff && token->basin_buff[i])
         {
-			int j = 0;
-			int param_len = 0;
-			while (token->basin_buff[i][j] != '\0')
-			{
-				if (token->basin_buff[i][j] == '$')
-				{
-					j++;
-					start = j;
-					printf("start: %d\n", start);
-					while (token->basin_buff[i][j] != '$' && token->basin_buff[i][j] != '\0')
-					{
-						//printf("[%c]\n", token->basin_buff[i][j]);
-						param_len++;
-						j++;
-					}
-					//printf("len: %d\n", param_len);
-					param = ft_substr(token->basin_buff[i], start, param_len);
-					printf("param before expansion: %s\n", param);
-					value = value_expansion(param, env);
-					free(param);
-					printf("param after expansion: %s\n", value);
-					if(value)
-					{
-						if (result == NULL)
-						{
-							result = ft_strdup("");
-						}
-						temp = result;
-						result = ft_strjoin(result, value);
-						free(temp);
-						printf("joined: %s\n", result);
-						//free(temp);
-						param = NULL;
-						value = NULL;
-					}
-				}
-				//j++;
-			}
-			if (result)
-			{
-				//free(param);
-				free(token->basin_buff[i]);  // Free the original string
-				token->basin_buff[i] = ft_strdup(result);  // Make a copy
-			}
-			free(result);
-			//free(temp);
-            //param = ft_strchr(token->basin_buff[i], '$');
-            //if (param)
-            //{
-            //    param = value_expansion(param + 1, env);
-            //    if(param)
-            //    {
-            //        free(token->basin_buff[i]);  // Free the original string
-            //        token->basin_buff[i] = ft_strdup(param);  // Make a copy
-            //    }
-            //    // printf("After expand: %s\n", param);
-            //}
-            // printf("param: %s\n", param);
+			//check single/double quote- need to expand or not
+			parameter_expansion(&token->basin_buff[i], env);
+			//handle exit code
+			//tilde epansion(optional)
+			//quote_removal
 			printf("token[%d][%d]: %s\n", token->index, i, token->basin_buff[i]);
             i++;
         }
         token = token->next;
     }
+}
+
+//void check_need_expansion()
+
+void parameter_expansion(char **str, char **env)
+{
+    char *param;
+	char *result;
+	int start;
+	int j;
+
+	param = NULL;
+	result = NULL;
+	start = 0;
+	j = 0;
+	while ((*str)[j] != '\0')
+	{
+		if ((*str)[j] == '$')
+		{
+			start = ++j;
+			//printf("start: %d\n", start);
+			while ((*str)[j] != '$' && (*str)[j] != '\0')
+				j++;
+			//printf("len: %d\n", param_len);
+			param = ft_substr((*str), start, j - start);
+			//printf("param before expansion: %s\n", param);
+			get_value(param, env, &result);
+		}
+		else
+			j++;
+	}
+	if (result)
+	{
+		free((*str));  // Free the original string
+		(*str) = result;  // Make a copy
+	}
+}
+
+void ft_free(char *str)
+{
+	free(str);
+	str = NULL;
+}
+
+void get_value(char *param, char **env, char **result)
+{
+	char *value;
+	char *temp;
+
+	value = NULL;
+	temp = NULL;
+	value = value_expansion(param, env);
+	free(param);
+	param = NULL;
+	//printf("param after expansion: %s\n", value);
+	if (value)
+	{
+		if (*result == NULL)
+			*result = ft_strdup("");
+		temp = *result;
+		*result = ft_strjoin(*result, value);
+		free(temp);
+		//printf("joined: %s\n", *result);
+		value = NULL;
+	}
+
 }
 
 //void find_dollar_sign(char *str)
@@ -124,16 +120,10 @@ char *value_expansion(char *param, char **env)
     printf("param: %s\n", param);
     while(env[i])
     {
-        //printf("env: %s\n", env[i]);
 		if (ft_strncmp(param, env[i], ft_strlen(param)) == 0)
         {
             value = getenv(param);
-			printf("value: %s\n", value);
-            //if ((*param + ft_strlen(param) + 1) != '\0')
-            //{
-            //    value = ft_strjoin(value, param);
-            //    printf("value: %s\n", value);
-            //}
+			//printf("value: %s\n", value);
             return(value);
         }
         i++;
