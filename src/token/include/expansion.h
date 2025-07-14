8e7efc52b8c936dfc17a2c56895b1f4c61ca6a5c
wshee <weiyee1401@gmail.com>
2025-07-14 22:28:22 +0800

    feat: check for quote whether need to expand or not

--- src/token/include/expansion.h
+++ src/token/include/expansion.h
@@ -1,6 +1,8 @@
 #ifndef EXPANSION_H
 # define EXPANSION_H
 
+#include <stdbool.h>
+
 void expand_tokens(t_token *token, char **env);
 
 #endif
--- src/token/src/expansion/expansion.c
+++ src/token/src/expansion/expansion.c
@@ -3,7 +3,7 @@
 void parameter_expansion(char **str, char **env);
 char *value_expansion(char *param, char **env);
 void get_value(char *param, char **env, char **result);
-
+bool check_need_expansion(char *str);
 
 void expand_tokens(t_token *token, char **env)
 {
@@ -13,10 +13,11 @@ void expand_tokens(t_token *token, char **env)
         while (token->basin_buff && token->basin_buff[i])
         {
 			//check single/double quote- need to expand or not
+			//if (check_need_expansion(token->basin_buff[i]) == 1)
 			parameter_expansion(&token->basin_buff[i], env);
-			//handle exit code
-			//tilde epansion(optional)
-			//quote_removal
+				//handle exit code
+				//tilde epansion(optional)
+				//quote_removal
 			printf("token[%d][%d]: %s\n", token->index, i, token->basin_buff[i]);
             i++;
         }
@@ -24,7 +25,36 @@ void expand_tokens(t_token *token, char **env)
     }
 }
 
-//void check_need_expansion()
+// if see double quote - need expand (return 1)
+// if see single quote - no need expand (return 0)
+// check to see if the quote is close
+ check_need_expansion(char *str)
+{
+	int i;
+	int count_dq;
+	int count_sq;
+
+	i = 0;
+	count_dq = 0;
+	count_sq = 0;
+	while (str[i])
+	{
+		if (str[i] == '"')
+		{
+			count_dq++;
+			if (count_dq % 2 == 0)
+				return (1);
+		}
+		else if (str[i] == '\'')
+		{
+			count_sq++;
+			if (count_sq % 2 == 0)
+				return (0);
+		}
+		i++;
+	}
+	return(0);
+}
 
 void parameter_expansion(char **str, char **env)
 {
@@ -32,13 +62,43 @@ void parameter_expansion(char **str, char **env)
 	char *result;
 	int start;
 	int j;
+	bool dq;
+	bool sq;
 
 	param = NULL;
 	result = NULL;
 	start = 0;
 	j = 0;
+	dq = false;
 	while ((*str)[j] != '\0')
 	{
+		if ((*str)[j] == '"' && dq == false)
+			dq = true;
+
+	// handle quote in str
+	// create a new function
+	// figure out how to handle both with quote or without quote
+		if ((*str)[j] == '"' || (*str)[j] == '\'' || (*str)[j] == '$')
+		{
+			if ((*str)[j] == '$')
+				handle_quote(*str, j, (*str)[j]);
+			else
+				handle_quote(*str, ++j, (*str)[j]);
+		}
+		while ((*str)[j] != '\0' && ((*str)[j] != quote))
+		{
+			if ((*str)[j] == '$' && quote == '"' || quote == '$')
+			{
+				start = ++j;
+				while (validEnvironVariableChar)
+					++j;
+				param = ft_substr((*str), start, j - start);
+				get_value(param, env, &result);
+			}
+			++j;
+		}
+	// end
+
 		if ((*str)[j] == '$')
 		{
 			start = ++j;
@@ -58,12 +118,6 @@ void parameter_expansion(char **str, char **env)
 		free((*str));  // Free the original string
 		(*str) = result;  // Make a copy
 	}
-}
-
-void ft_free(char *str)
-{
-	free(str);
-	str = NULL;
 }
 
 void get_value(char *param, char **env, char **result)
