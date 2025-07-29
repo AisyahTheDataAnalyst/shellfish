c299484f2a648801438c1244ee07547410b08168
wyshee14 <wshee@u82z01s06.42kl.edu.my>
2025-07-29 21:46:13 +0800

    fix: norminette

--- src/execution/exec/word/command/exec_word.c
+++ src/execution/exec/word/command/exec_word.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_word.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: wshee <wshee@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 10:07:12 by aimokhta          #+#    #+#             */
-/*   Updated: 2025/07/27 16:29:38 by aimokhta         ###   ########.fr       */
+/*   Updated: 2025/07/29 21:44:14 by wshee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void		exec_word(t_ast *ast, t_exc *exc);
 static void	fork_error_message_tokenword(t_exc *exc);
 static void	waiting_exec_word(pid_t pid, int exit_status, t_exc *exc);
 
-// In practice, most shells just reapply SIG_IGN before every fork 
+// In practice, most shells just reapply SIG_IGN before every fork
 // because it's cleaner, safer, and avoids strange bugs.
 void	exec_word(t_ast *ast, t_exc *exc)
 {
@@ -31,7 +31,7 @@ void	exec_word(t_ast *ast, t_exc *exc)
 		signal(SIGINT, SIG_IGN);
 		pid = fork();
 		if (pid < 0)
-			return (fork_error_message_tokenword(exc)); 
+			return (fork_error_message_tokenword(exc));
 		if (pid == 0)
 		{
 			dup2_close_infile_outfile(exc);
@@ -49,19 +49,19 @@ static void	fork_error_message_tokenword(t_exc *exc)
 	return ;
 }
 
-// WIFEXITED(status) 
+// WIFEXITED(status)
 //	= returns true - child exited normally: using exit() / return from main()
-// WIFEXITED(status) 
+// WIFEXITED(status)
 //	= returns false - child killed by signal (SIGINT / SIGSEGV)
-// WEXITSTATUS (used only when WIFEXITED(status) is true) 
-//	= the exit code the child returned 
-// WIFSIGNALED(status) 
+// WEXITSTATUS (used only when WIFEXITED(status) is true)
+//	= the exit code the child returned
+// WIFSIGNALED(status)
 //	= returns true - child killed by smthg (SIGINT / kill() / SIGSEGV)
-// WTERMSIG(status) (used only if WIFSIGNALED(status) is true) 
-//	= what signal killed the child 
+// WTERMSIG(status) (used only if WIFSIGNALED(status) is true)
+//	= what signal killed the child
 // ( == 0) ---> false
 // ( != 0) ---> true
-// we need WIFSIGNALED coz we dont have signal_handler function 
+// we need WIFSIGNALED coz we dont have signal_handler function
 // like (SIG_DFL / SIG_IGN)
 static void	waiting_exec_word(pid_t pid, int exit_status, t_exc *exc)
 {
